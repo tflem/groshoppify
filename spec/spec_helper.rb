0b525ab23f6ce4e82f1ef3626f962a3dae6a15d9
Tim Fleming <tfleming26@hotmail.com>
2021-02-11 17:52:06 -0600

    Add filters for simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,10 +15,14 @@
 require 'simplecov'
 require 'simplecov-console'
 
-SimpleCov.formatter = SimpleCov.formatter =
-  SimpleCov::Formatter::Console
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::Console,
+])
 
-SimpleCov.start
+SimpleCov.start do
+  add_filter "/spec/"
+end
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
