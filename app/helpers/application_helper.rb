619a48b72504a5120688205625408e06e4c00aca
Tim Fleming <tfleming26@hotmail.com>
2021-02-16 19:47:29 -0600

    Create title functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def title(*items)
+    content_for :title do
+      (items << 'Groshoppify').join(' - ')
+    end
+    return content_for :title unless items.empty?
+  end
 end
--- spec/features/creating_products_spec.rb
+++ spec/features/creating_products_spec.rb
@@ -12,5 +12,11 @@ RSpec.feature "Our users can create new products" do
 		click_button "Create Product"
 
 		expect(page).to have_content "Product information has been created."
+
+		product = Product.find_by(name: "Milk")
+		expect(page.current_url).to eq product_url(product)
+
+		title = "Milk - Products - Groshoppify"
+		expect(page).to have_title title
   end
 end
