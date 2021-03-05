888ec85ec4da2f55294f6590c047dfa8b32bdfea
Tim Fleming <tfleming26@hotmail.com>
2021-03-05 10:26:39 -0600

    Users can view notes within a product

---
+++ spec/factories/notes.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :note do
+    name { "Example note" }
+    description { "An example note--no more, no less" }
+  end
+end
---
+++ spec/features/viewing_notes_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'Our users can view notes' do
+	before do
+    milk = FactoryBot.create(:product, name: 'Milk')
+    
+    FactoryBot.create(:note, product: milk,
+      name: 'No Skim!',
+      description: 'Pickup 2%, not skim milk!')
+
+    chips = FactoryBot.create(:product, name: "Ketchup Chips")
+    
+    FactoryBot.create(:note, product: chips,
+      name: 'Name Brand Chips',
+      description: 'Get the top brand, not el cheapo.')
+
+    visit '/'
+  end
+
+  scenario 'for any product' do
+    click_link 'Milk'
+
+
+    expect(page).to have_content 'No Skim!'
+    expect(page).to_not have_content 'Name Brand Chips'
+
+
+    click_link 'No Skim!'
+    within('.note h2') do
+      expect(page).to have_content 'No Skim!'
+    end
+
+
+    expect(page).to have_content 'Pickup 2%, not skim milk!'
+  end
+end
+
