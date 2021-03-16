93480cc40c778b044db919a4f9fe552d9406fc0e
Tim Fleming <tfleming26@hotmail.com>
2021-03-16 15:39:01 -0600

    Cleanup styling file, fix headers in a number of views, and missing buttons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,7 +18,6 @@ const images = require.context('../images', true)
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-//require('stylesheets/application.scss')
 
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
