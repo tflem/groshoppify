015a1d1ce1688b7e3acd1f8fc068c9e388d9a63f
Tim Fleming <tfleming26@hotmail.com>
2021-03-30 15:56:30 -0600

    Only admins can create and delete products

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
     end
     return content_for :title unless items.empty?
   end
+
+  def admins_only(&block)
+  	block.call if current_user.try(:admin?)
+  end
 end
---
+++ spec/features/hidden_links_spec.rb
@@ -0,0 +1,47 @@
+require "rails_helper"
+
+RSpec.describe "Our users can only see appropriate links" do
+  let(:product) { FactoryBot.create(:product) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:admin) { FactoryBot.create(:user, :admin) }
+
+  context "lurkers" do
+    it "cannot see the New Product link" do
+      visit "/"
+      expect(page).not_to have_link "New Product"
+    end
+
+    it "cannot see tge Delete Product link" do
+      visit product_path(product)
+      expect(page).not_to have_link "Delete Product"      
+    end
+  end
+
+  context "general users" do
+    before { login_as(user) }
+
+    it "cannot see the New Product link" do
+      visit "/"
+      expect(page).not_to have_link "New Product"
+    end
+
+    it "cannot see tge Delete Product link" do
+      visit product_path(product)
+      expect(page).not_to have_link "Delete Product"      
+    end
+  end
+
+  context "admin users" do
+    before { login_as(admin) }
+
+    it "can see the New Product link" do
+      visit "/"
+      expect(page).to have_link "New Product"
+    end
+
+    it "can see the Delete Product link" do
+      visit product_path(product)
+      expect(page).to have_link "Delete Product"      
+    end
+  end
+end
